3ba4e164e65264c294d0af58e5159f05129a7570
Builder <Builder@MP-LAPTOP.com>
2019-11-19 23:19:22 +0000

    Dockerfile cleanup

--- helloworld/src/main/java/com/home/helloworld/HelloWorld.java
+++ helloworld/src/main/java/com/home/helloworld/HelloWorld.java
@@ -1,12 +1,17 @@
 package com.home.helloworld;
-/* This is a simple Java program taken from the internet
-   FileName : "HelloWorld.java". */
+/* This is a simple Java program taken from an internet example
+   FileName : "HelloWorld.java". 
+   To run the java file cd to the root of the classes folder e,g, for java/com/home/helloworld, cd to the java folder then run the following command:
+   java com.home.helloworld.HelloWorld
+   */
+   
+   
 public class HelloWorld 
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
+    public static void main(String[] args) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello World!"); 
     } 
 } 
